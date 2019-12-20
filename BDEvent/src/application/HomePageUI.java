b885c214b49b7ff308721ae5361947a3bb8486bc
Clemclem74 <clementemil@aol.fr>
2019-12-20 23:13:48 +0100

    database update

--- BDEvent/src/application/HomePageUI.java
+++ BDEvent/src/application/HomePageUI.java
@@ -6,7 +6,7 @@ import buisnessLogic.User;
 
 import java.net.URL;
 import java.util.ResourceBundle;
- 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -32,10 +32,10 @@ public class HomePageUI extends Routing implements Initializable {
 	 @FXML
 	 private Label email_text;
 	 @FXML
-	 private Label phone_number_text;  
+	 private Label phone_number_text;
 	 @FXML
 	 private Label name_text;
-	 
+
 
 	@Override
 	   public void initialize(URL location, ResourceBundle resources) {
@@ -54,55 +54,40 @@ public class HomePageUI extends Routing implements Initializable {
 			  events_button.setVisible(false);
 			  quitBDEbutton.setVisible(false);
 			  communication_button.setVisible(false);
-			  
+
 		  }
 	       // TODO (don't really need to do anything here).
 	   }
-	 
+
 	   // When user click on myButton
 	   // this method will be called.S
-<<<<<<< HEAD
-	  
-=======
-	   public void HomePageAction(ActionEvent event) {
-//		  User user=super.getCurrentUser();
-//		  System.out.println("Email : " + user.getEmailuser());
-//	      this.username_text.setText(user.getUsername());
-//		  this.email_text.setText(user.getEmailuser());
-//	      this.phone_number_text.setText(user.getPhonenumberuser());
-//		  this.name_text.setText(user.getFirstname() +" "+ user.getLastname());
-	   }
-	   
-	   // When user click on logoutButton
-	   // this method will be called.
->>>>>>> origin/master
 	   public void logout(ActionEvent event) {
 		   Routing.setCurrentUser(null);
 		   Routing root = new Routing();
 		   root.logout();
 	   }
-	   
+
 	   // When user click on modifyUserButton
 	   // this method will be called.
 	   public void modifyUser(ActionEvent event) {
 		   Routing root = new Routing();
 		   root.modifyUser();
 	   }
-	   
+
 	   // When user click on deleteAccountButton
 	   // this method will be called.
 	   public void deleteUser(ActionEvent event) {
 		   Routing root = new Routing();
 		   root.deleteUser();
 	   }
-	   
+
 	   // When user click on createBdeButton
 	   // this method will be called.
 	   public void createBDE(ActionEvent event) {
 		   Routing root = new Routing();
 		   root.createBDE();
 	   }
-	   
+
 	   public void quitBDE(ActionEvent event) {
 		   UserFacade userfacade = new UserFacade();
 		   userfacade.setBDEnull(super.getCurrentUser());
