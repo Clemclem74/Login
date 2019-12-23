04a6393d8d6192676478160467632a68d3772479
MatthewHaas19 <44777911+MatthewHaas19@users.noreply.github.com>
2019-12-23 19:40:48 +0100

    adding event in sql

--- database.sql
+++ database.sql
@@ -1,6 +1,22 @@
 --------------------------------------------------------
 --  Fichier créé - lundi-décembre-23-2019   
 --------------------------------------------------------
+
+
+ CREATE TABLE "SYSTEM"."EVENT" 
+   (	"EVENT" NUMBER, 
+	"TITLE" VARCHAR2(20 BYTE), 
+	"DESCRIPTION" VARCHAR2(50 BYTE), 
+	"DATE" VARCHAR2(50 BYTE)
+   ) PCTFREE 10 PCTUSED 40 INITRANS 1 MAXTRANS 255 
+ NOCOMPRESS LOGGING
+  STORAGE(INITIAL 65536 NEXT 1048576 MINEXTENTS 1 MAXEXTENTS 2147483645
+  PCTINCREASE 0 FREELISTS 1 FREELIST GROUPS 1
+  BUFFER_POOL DEFAULT FLASH_CACHE DEFAULT CELL_FLASH_CACHE DEFAULT)
+  TABLESPACE "SYSTEM" ;
+
+
+
 --------------------------------------------------------
 --  DDL for Table BDE
 --------------------------------------------------------
