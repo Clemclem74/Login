e4c00c4a65fe95376155d87a5da62f1fa0b0330f
Clemclem74 <clementemil@aol.fr>
2020-01-11 20:15:24 +0100

    css finished

--- BDEvent/src/dao/OracleDAO.java
+++ BDEvent/src/dao/OracleDAO.java
@@ -14,17 +14,17 @@ import buisnessLogic.User;
 public abstract class OracleDAO<T> {
   protected Connection connect = null;
 
-  /*
+
   public static final String ORACLE_DB_PATH = "jdbc:oracle:thin:@localhost:1521:xe";
   public static final String ORACLE_DB_USER = "system";
   public static final String ORACLE_DB_PASSWORD = "oose";
 
-  */
 
 
-  public static final String ORACLE_DB_PATH = "jdbc:mysql://localhost/oose";
-  public static final String ORACLE_DB_USER = "root";
-  public static final String ORACLE_DB_PASSWORD = "";
+
+//  public static final String ORACLE_DB_PATH = "jdbc:mysql://localhost/oose";
+//  public static final String ORACLE_DB_USER = "root";
+//  public static final String ORACLE_DB_PASSWORD = "";
 
 
 
